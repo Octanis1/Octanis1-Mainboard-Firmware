5fa91e206632f3f2e16e97c9e016656366236860
Sam Sulaimanov <sam@bitmorse.com>
2015-04-26 08:31:31 +0000

    Adding cli prototypes. Not going for TIs instrumentation but implementing simple cli console with uart.

--- src/core/cli.c
+++ src/core/cli.c
@@ -4,3 +4,28 @@
  *  Author:
  */
 #include "cli.h"
+
+//which uart index to use for the CLI
+#define CLI_UART 0
+//buffer to fill before data goes out
+#define CLI_BUFFER 512
+//max number of commands
+#define CLI_MAXCOMMANDS 10
+
+static void* cli_commands[CLI_MAXCOMMANDS][2];
+
+
+void cli_init(){
+
+
+}
+
+//register a command line command here. will be called when "commandstring [argument]" was typed into cli.
+void cli_register_command(void (*command_cb)(char*), char* commandstring){
+
+}
+
+//runs with lowest priority
+void cli_task(){
+
+}
--- src/core/cli.h
+++ src/core/cli.h
@@ -3,3 +3,14 @@
  *  Description: Provides a command line interface and offers possibility to register function as new command
  *  Author:
  */
+
+
+//setup uart, connect with stdin and stdout
+void cli_init();
+
+//register a command line command here. will be called when "commandstring [argument]" was typed into cli.
+void cli_register_command(void (*command_cb)(char*), char* commandstring);
+
+//runs with lowest priority
+void cli_task();
+
